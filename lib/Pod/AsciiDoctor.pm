d11d3581c69cae070ce029391fe93e2009185529
Shlomi Fish <shlomif@shlomifish.org>
2023-01-29 17:12:17 +0200

    convert to dzil

--- lib/Pod/AsciiDoctor.pm
+++ lib/Pod/AsciiDoctor.pm
@@ -17,7 +17,6 @@ Version 0.01
 
 our $VERSION = '0.1';
 
-
 =head1 SYNOPSIS
 
 Converts the POD of a Perl module to AsciiDoc format.
@@ -34,7 +33,8 @@ Converts the POD of a Perl module to AsciiDoc format.
 
 =cut
 
-sub initialize {
+sub initialize
+{
     my $self = shift;
     $self->SUPER::initialize(@_);
     $self->_prop;
@@ -45,24 +45,26 @@ sub initialize {
 
 =cut
 
-sub adoc {
+sub adoc
+{
     my $self = shift;
     my $data = $self->_prop;
-    return join "\n", @{$data->{text}};
+    return join "\n", @{ $data->{text} };
 }
 
 =head2 _prop
 
 =cut
 
-sub _prop {
+sub _prop
+{
     my $self = shift;
-    $self->{prop} //=  {
-        'text' => [],
-        'headers' => "",
+    $self->{prop} //= {
+        'text'       => [],
+        'headers'    => "",
         'topheaders' => {},
-        'command' => '',
-        'indent' => 0
+        'command'    => '',
+        'indent'     => 0
     };
 }
 
@@ -70,9 +72,10 @@ sub _prop {
 
 =cut
 
-sub _sanitise {
+sub _sanitise
+{
     my $self = shift;
-    my $p = shift;
+    my $p    = shift;
     chomp($p);
     return $p;
 }
@@ -81,43 +84,50 @@ sub _sanitise {
 
 =cut
 
-sub append {
-    my ($self, $doc) = @_;
+sub append
+{
+    my ( $self, $doc ) = @_;
     my $data = $self->_prop;
-    push @{$data->{text}}, $doc;
+    push @{ $data->{text} }, $doc;
 }
-    
-    
+
 =head2 command
 
     Overrides Pod::Parser::command
 
 =cut
-    
-sub command {
-    my ($self, $command, $paragraph, $lineno) = @_;
+
+sub command
+{
+    my ( $self, $command, $paragraph, $lineno ) = @_;
     my $data = $self->_prop;
     $data->{command} = $command;
+
     # _sanitise: Escape AsciiDoctor syntax chars that appear in the paragraph.
     $paragraph = $self->_sanitise($paragraph);
 
-    if ($command =~ /head(\d)/) {
+    if ( $command =~ /head(\d)/ )
+    {
         my $level = $1;
         $level //= 2;
         $data->{command} = 'head';
-        $data->{topheaders}{$1} = defined($data->{topheaders}{$1}) ? $data->{topheaders}{$1}++ : 1;
+        $data->{topheaders}{$1} =
+            defined( $data->{topheaders}{$1} ) ? $data->{topheaders}{$1}++ : 1;
         $paragraph = $self->set_formatting($paragraph);
-        $self->append($self->make_header($command, $level, $paragraph));
+        $self->append( $self->make_header( $command, $level, $paragraph ) );
     }
 
-    if ($command =~ /over/) {
+    if ( $command =~ /over/ )
+    {
         $data->{indent}++;
     }
-    if ($command =~ /back/) {
+    if ( $command =~ /back/ )
+    {
         $data->{indent}--;
     }
-    if ($command =~ /item/) {
-        $self->append($self->make_text($paragraph, 1));
+    if ( $command =~ /item/ )
+    {
+        $self->append( $self->make_text( $paragraph, 1 ) );
     }
     return;
 }
@@ -127,9 +137,10 @@ sub command {
     Overrides Pod::Parser::verbatim
 
 =cut
-    
-sub verbatim {
-    my $self = shift;
+
+sub verbatim
+{
+    my $self      = shift;
     my $paragraph = shift;
     chomp($paragraph);
     $self->append($paragraph);
@@ -141,10 +152,11 @@ sub verbatim {
     Overrides Pod::Parser::textblock
 
 =cut
-    
-sub textblock {
+
+sub textblock
+{
     my $self = shift;
-    my ($paragraph, $lineno) = @_;
+    my ( $paragraph, $lineno ) = @_;
     chomp($paragraph);
     $paragraph = $self->interpolate($paragraph);
     $self->append($paragraph);
@@ -156,32 +168,44 @@ sub textblock {
 
 =cut
 
-sub interior_sequence { 
-    my ($parser, $seq_command, $seq_argument) = @_;
+sub interior_sequence
+{
+    my ( $parser, $seq_command, $seq_argument ) = @_;
     ## Expand an interior sequence; sample actions might be:
-    return "*$seq_argument*"     if ($seq_command eq 'B');
-    return "`$seq_argument`"     if ($seq_command eq 'C');
-    return "_${seq_argument}_'"  if ($seq_command eq 'I' || $seq_command eq 'F');
-    if ($seq_command eq 'L') {
+    return "*$seq_argument*" if ( $seq_command eq 'B' );
+    return "`$seq_argument`" if ( $seq_command eq 'C' );
+    return "_${seq_argument}_'"
+        if ( $seq_command eq 'I' || $seq_command eq 'F' );
+    if ( $seq_command eq 'L' )
+    {
         my $ret = "";
         my $text;
         my $link;
-        if ($seq_argument =~ /(.+)\|(.+)/) {
+        if ( $seq_argument =~ /(.+)\|(.+)/ )
+        {
             $text = $1;
             $link = $2;
-        } elsif ($seq_argument =~ /(.+)/) {
+        }
+        elsif ( $seq_argument =~ /(.+)/ )
+        {
             $text = "";
             $link = $1;
         }
-        if ($link =~ /(.+?\:\/\/)(.+)/) {
+        if ( $link =~ /(.+?\:\/\/)(.+)/ )
+        {
             $ret .= "$link";
-            $ret .= " [$text]" if (length($text));
-        } elsif (length($link)) {
+            $ret .= " [$text]" if ( length($text) );
+        }
+        elsif ( length($link) )
+        {
             # Internal link
-            if ($link =~ /(.+)\/(.+)/) {
+            if ( $link =~ /(.+)\/(.+)/ )
+            {
                 $ret = "<< $1#$2 >>";
                 $ret = "<< $1#$2,$text >>" if ($text);
-            } else {
+            }
+            else
+            {
                 $ret = "<< $link >>";
                 $ret = "<< $link,$text >>" if ($text);
             }
@@ -193,13 +217,17 @@ sub interior_sequence {
 =head2 make_header
 
 =cut
-    
-sub make_header {
-    my ($self, $command, $level, $paragraph) = @_;
-    if ($command =~ /head/) {
-        my $h = sprintf("%s %s", "=" x ($level+1), $paragraph);
+
+sub make_header
+{
+    my ( $self, $command, $level, $paragraph ) = @_;
+    if ( $command =~ /head/ )
+    {
+        my $h = sprintf( "%s %s", "=" x ( $level + 1 ), $paragraph );
         return $h;
-    } elsif ($command =~ /item/) {
+    }
+    elsif ( $command =~ /item/ )
+    {
         return "* $paragraph";
     }
 }
@@ -207,14 +235,16 @@ sub make_header {
 =head2 make_text
 
 =cut
-    
-sub make_text {
-    my ($self, $paragraph, $list) = @_;
+
+sub make_text
+{
+    my ( $self, $paragraph, $list ) = @_;
     my @lines = split "\n", $paragraph;
-    my $data = $self->_prop;
+    my $data  = $self->_prop;
     my @i_paragraph;
     my $pnt = $list ? "*" : "";
-    for my $line (@lines) {
+    for my $line (@lines)
+    {
         push @i_paragraph, $pnt x $data->{indent} . " " . $line . "\n";
     }
     return join "\n", @i_paragraph;
@@ -223,9 +253,10 @@ sub make_text {
 =head2 set_formatting
 
 =cut
-    
-sub set_formatting {
-    my $self = shift;
+
+sub set_formatting
+{
+    my $self      = shift;
     my $paragraph = shift;
     $paragraph =~ s/I<(.*)>/_$1_/;
     $paragraph =~ s/B<(.*)>/*$1*/;
@@ -233,7 +264,7 @@ sub set_formatting {
     $paragraph =~ s/C<(.*)>/\`$1\`/xms;
     return $paragraph;
 }
-    
+
 =head1 AUTHOR
 
 Balachandran Sivakumar, C<< <balachandran at balachandran.org> >>
@@ -286,7 +317,7 @@ L<http://search.cpan.org/dist/Pod-AsciiDoctor/>
 Copyright 2015 Balachandran Sivakumar, Abhisek Kumar Rout.
 
 This program is free software; you can redistribute it and/or modify it
-under the terms of the the Apache License (2.0). You can get a copy of 
+under the terms of the the Apache License (2.0). You can get a copy of
 the license at http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing,
